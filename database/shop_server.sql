709ac8f55eee29054e140dcba58a740e329349f9
Ivan Osipchyk <ivanosipchyk@Ivans-MacBook-Air.local>
2023-04-12 15:29:43 +0300

    added registration procedures

--- database/shop_server.sql
+++ database/shop_server.sql
@@ -100,20 +100,101 @@ create table products_storages(
 insert into admins(first_name, last_name, email, phone_number, password_, reference_code)
 values ('Initial', 'Admin', 'initial.admin@project.com', '+420776119548', 'password', 'ref111');
 
--- create database test;
--- use test;
---
--- create table user_(
---     user_id int primary key,
---     first_name varchar(20) not null,
---     last_name varchar(20) not null,
---     email varchar(50) not null,
---     phone varchar(20) not null,
---     country varchar(20) not null,
---     city varchar(20) not null,
---     street varchar(20) not null,
---     house_number int not null,
---     postal_code varchar(10) not null,
---     password_ varchar(20) not null,
---     admin_code int not null
--- );
+use shop;
+
+-- procedure for finding old address or inserting a new one
+
+create procedure find_or_insert_address(
+  in country_ varchar(20),
+  in city_ varchar(20),
+  in street_ varchar(30),
+  in house_number_ varchar(10),
+  in postal_code_ varchar(10),
+  out result_id int
+)
+begin
+  declare address_id_ int;
+
+  -- check if address already exists
+  select address_id into address_id_ from addresses where country = country_ and city = city_ and street = street_ and house_number = house_number_ and postal_code = postal_code_;
+
+  if address_id_ is null then
+    -- if address does not exist, insert a new row and get the generated id
+    insert into addresses (country, city, street, house_number, postal_code) VALUES (country_, city_, street_, house_number_, postal_code_);
+    set result_id = LAST_INSERT_ID();
+  else
+    -- if address already exists, return its id
+    set result_id = address_id_;
+  end if;
+end;
+
+-- procedure for customer registration
+
+create procedure customer_registration(
+    in first_name_ varchar(20),
+    in last_name_ varchar(20),
+    in email_ varchar(50),
+    in phone_number_ varchar(15),
+    in password__ varchar(1000),
+    in country_ varchar(20),
+    in city_ varchar(20),
+    in street_ varchar(30),
+    in house_number_ varchar(10),
+    in postal_code_ varchar(10),
+    out customer_registered int
+)
+begin
+    declare customer_exists int;
+    -- check if customer exists
+    select customer_id into customer_exists from customers where email = email_;
+
+    -- if does not exist
+    if customer_exists is null then
+        -- insert address and get its id
+        call find_or_insert_address(country_, city_, street_, house_number_, postal_code_);
+        select @result_id;
+        -- create new customer
+        insert into customers(first_name, last_name, email, phone_number, password_, address_id)
+            values ( first_name_, last_name_, email_, phone_number_, password__, @result_id);
+        set customer_registered = 1;
+    else
+        set customer_exists = 0;
+    end if;
+end;
+
+-- procedure for admin registration
+
+create procedure admin_registration(
+    in first_name_ varchar(20),
+    in last_name_ varchar(20),
+    in email_ varchar(50),
+    in phone_number_ varchar(15),
+    in password__ varchar(100),
+    in reference_code_ varchar(10),
+    out admin_registered varchar(30)
+)
+begin
+    declare admin_exists int;
+    declare reference_code_exists varchar(10);
+
+    -- check if admin exists
+    select admin_id into admin_exists from admins where email = email_;
+
+    -- if does not exists
+    if admin_exists is null then
+        -- check if reference code is valid
+        select reference_code into reference_code_exists from admins where reference_code = reference_code_;
+
+        -- if valid
+        if reference_code_exists is not null then
+            -- register admin
+            insert into admins(first_name, last_name, email, phone_number, password_)
+                values (first_name_, last_name_, email_, phone_number_, password__);
+            set admin_registered = 'Admin registered';
+        else
+            set admin_registered = 'Invalid reference code';
+        end if;
+    else
+        set admin_registered = 'Admin already exists';
+    end if;
+end;
